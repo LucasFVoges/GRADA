9c9ed6e5ba6322f94eaf8c564921cb8f4044de78
Lucas F. Voges <lucas.voges@chemie.uni-hamburg.de>
2022-03-23 09:43:36 +0100

    idk

--- R/grada_python_functions.R
+++ R/grada_python_functions.R
@@ -1,6 +1,8 @@
-#' GRADA - Python
+#' GRADA - Python - RC Adapters 
 #'
-#' This function will reverse complement all adapters (if they have standard bases)
+#' This function will reverse complement all adapters
+#' 
+#' Will work with DNA bases only (A,T,C,G,N)
 #' 
 #' @param file Path to the adapter file
 #' @export
@@ -15,4 +17,19 @@ grada_py_make_reverse_complement <- function(file){
   #     cat("To Field Empty") } } 
   # 
   
-  }
+}
+
+#' GRADA - Python - average read length
+#'
+#' This function will return the average read length for a standard .fastq file as a .txt file.
+#' 
+#' The first 100 reads will be evaluated (if not set to another parameter)
+#' 
+#' @param file Path to the fastq file
+#' @param sample number of reads to look at (std. 100)
+#' @export
+grada_py_AVG_LengthOfReads <- function(file, sample = 100){
+  path <- paste(system.file(package="GRADA"), "AVG_LengthOfReads.py", sep="/")
+  command <- paste("python", path, "-f", file, "-nb", sample, sep = " ")
+  try(suppressWarnings(response <- system(command, intern=TRUE)), silent = TRUE)
+}
